c23479916e3d05c22ed3f972ba843add1aa4299a
nik <amaravadi.sainikhil@gmail.com>
2024-04-04 23:22:07 -0500

    feat: added Scheduler for sending mails

---
+++ backend/blind75/src/main/java/com/niks/blind75/scheduler/Scheduler.java
@@ -0,0 +1,30 @@
+package com.niks.blind75.scheduler;
+
+import com.niks.blind75.service.TriggerService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.AutoConfigureOrder;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.Trigger;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
+
+import java.text.SimpleDateFormat;
+
+@EnableScheduling
+@Configuration
+public class Scheduler {
+
+    @Autowired
+    TriggerService triggerService;
+    @Scheduled(cron = "* * * * * *")
+    public void everyMinuteJob(){
+        System.out.println("Hello" + java.time.LocalDate.now());
+    }
+
+    @Scheduled(cron = "0 0 6 * * *")
+    public void mailScheduler(){
+        System.out.println("Before:");
+        triggerService.triggerEmails();
+        System.out.println(("Done"));
+    }
+}
--- backend/blind75/src/main/java/com/niks/blind75/service/TriggerService.java
+++ backend/blind75/src/main/java/com/niks/blind75/service/TriggerService.java
@@ -126,4 +126,6 @@ public class TriggerService {
         }
         return null;
     }
+
+
 }

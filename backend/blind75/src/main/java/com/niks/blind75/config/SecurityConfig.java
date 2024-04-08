83d91e4d7b976381bc38e613a6a51afa01bd04e1
nik <amaravadi.sainikhil@gmail.com>
2024-04-07 22:56:44 -0500

    feat: test code for oauth2 to secure api

---
+++ backend/blind75/src/main/java/com/niks/blind75/config/SecurityConfig.java
@@ -0,0 +1,24 @@
+package com.niks.blind75.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .dispatcherTypeMatchers(HttpMethod.valueOf("/public")).permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .oauth2Login();
+        return null;
+    }
+}
